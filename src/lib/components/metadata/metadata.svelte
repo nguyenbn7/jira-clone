1eeea910a3dcb28e7449030ff397b69a7f9b7491
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-04-01 21:44:58 +0700

    init project

---
+++ src/lib/components/metadata/metadata.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { PUBLIC_APP_NAME } from '$env/static/public';
+
+	interface Props {
+		title?: string;
+	}
+
+	let { title }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>{title ? `${title} - ${PUBLIC_APP_NAME}` : `${PUBLIC_APP_NAME}`}</title>
+</svelte:head>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	import '../app.css';
+
+	interface LayoutProps {
+		data: LayoutData;
+		children: Snippet;
+	}
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+{@render children()}
